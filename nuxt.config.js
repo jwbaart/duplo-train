18901bbebe7278dce6b59d25c06880d3cf806fa2
Jan-Willem Baart <jwbaart@gmail.com>
2019-07-30 14:03:48 +0200

    feat(): google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,7 +40,8 @@ export default {
    */
   modules: [
     // Doc: https://bootstrap-vue.js.org/docs/
-    'bootstrap-vue/nuxt'
+    'bootstrap-vue/nuxt',
+    ['@nuxtjs/google-analytics', { ua: '135688161-2' }]
   ],
   /*
    ** Build configuration

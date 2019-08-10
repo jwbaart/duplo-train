d1d5e6011684b175f392c393defd6c364a9c013e
Jan-Willem Baart <jwbaart@gmail.com>
2019-08-10 22:18:10 +0200

    fix(): floating action buttons
    Always show on top

--- pages/index.vue
+++ pages/index.vue
@@ -4,15 +4,15 @@
       <v-progress-circular indeterminate size="64"></v-progress-circular>
     </v-overlay>
 
-    <v-layout>
-      <v-flex xs12>
-        <TrainSearch />
-      </v-flex>
-    </v-layout>
-
     <v-layout wrap>
       <v-flex v-for="train in trains" :key="train.name" xs12 sm6 md4>
         <TrainConductor :train="train" />
+      </v-flex>
+    </v-layout>
+
+    <v-layout>
+      <v-flex xs12>
+        <TrainSearch />
       </v-flex>
     </v-layout>
   </v-container>

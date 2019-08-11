5f3d704f1e688e81b249289ab0402aa2ccee771b
Jan-Willem Baart <jwbaart@gmail.com>
2019-08-11 15:15:47 +0200

    chore(): site title & icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,32 @@ export default {
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      {
+        rel: 'apple-touch-icon',
+        href: '/apple-touch-icon.png',
+        sizes: '180x180'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon-32x32.png',
+        sizes: '32x32'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon-32x32.png',
+        sizes: '32x32'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon-16x16.png',
+        sizes: '16x16'
+      },
+      { rel: 'manifest', href: '/site.webmanifest' }
+    ]
   },
   /*
    ** Customize the progress-bar color
